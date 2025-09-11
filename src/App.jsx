37fefcc1a88e41c2885c15806db681ead4050642
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-09-11 16:08:13 +0200

    added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { CV } from "./components/CV";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="cv">
       <CV />
+      <Analytics />
     </div>
   );
 }
